c0647436d62202c13dd1bad17a01e1bbe5121821
Majster96PL <piotruniok96@gmail.com>
2022-06-27 12:06:41 +0200

    Add methods for create new record, update and delete record from table

--- src/main/java/com/example/demo/training/TrainingService.java
+++ src/main/java/com/example/demo/training/TrainingService.java
@@ -1,9 +1,11 @@
 package com.example.demo.training;
 
+import com.example.demo.exceprion.TrainingException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +14,33 @@ public class TrainingService {
     private final TrainingRepository trainingRepository;
     public List<Training> getAllTraining() {
         return trainingRepository.findAll();
+    }
+
+    public Training saveTraining(Training training){
+        Training newRecord = new Training(
+                training.getKilometres(),
+                training.getTrainingDate(),
+                training.getKCal(),
+                training.getTime(),
+                training.getNoteText()
+        );
+        return trainingRepository.save(newRecord);
+    }
+
+    public Training updateTraining(Long id){
+        Optional<Training> optionalTraining = trainingRepository.findById(id);
+        Training training = null;
+
+        if(optionalTraining.isPresent()){
+            training = optionalTraining.get();
+        }else{
+            throw new TrainingException("Training not found!");
+        }
+        return training;
+    }
+
+    public void deleteTraining(Long id){
+        trainingRepository.deleteById(id);
     }
 
 
--- src/main/java/com/example/demo/web/controller/TrainingController.java
+++ src/main/java/com/example/demo/web/controller/TrainingController.java
@@ -1,10 +1,12 @@
 package com.example.demo.web.controller;
 
+import com.example.demo.training.Training;
 import com.example.demo.training.TrainingService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @AllArgsConstructor
@@ -15,5 +17,31 @@ public class TrainingController {
     public String training(Model model){
         model.addAttribute("listTraining", trainingService.getAllTraining());
         return "training";
+    }
+
+    @GetMapping("/new")
+    public String addNewTraining(Model model){
+        Training training = new Training();
+        model.addAttribute("training", training );
+        return "new";
+    }
+
+    @PostMapping("/save")
+    public String saveTraining(@ModelAttribute("training") Training training){
+        trainingService.saveTraining(training);
+        return "redirect:/training";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showFormForUpdate(@PathVariable ( value = "id") Long id, Model model) {
+        Training training = trainingService.updateTraining(id);
+        model.addAttribute("employee", training);
+        return "update";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTraining(@PathVariable(value ="id")Long id){
+        trainingService.deleteTraining(id);
+        return "redirect:/training";
     }
 }
